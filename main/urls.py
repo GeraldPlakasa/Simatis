f5bbd92a06f32b2d48f445432db0a6f39b8813ba
Owner <spanixgerald@gmail.com>
2021-12-26 22:32:05 +0700

    create structure page

--- main/urls.py
+++ main/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.main, name='main'),
     path('data/', views.data),
+    path('struktur/', views.struktur),
 ]
--- main/views.py
+++ main/views.py
@@ -2,8 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def index(request):
-    return render(request, 'index.html')
+def main(request):
+    return render(request, 'main.html')
 
 def data(request):
     return render(request, 'data.html')
+
+def struktur(request):
+    return render(request, 'struktur.html')
